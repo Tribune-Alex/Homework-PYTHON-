5ba3b7dc1cc4b8527f780174b0c0915348012817
Alex Todua <alx.todua777@gmail.com>
2025-11-04 21:06:53 +0400

    Add modified file

--- HomeWork#14/students.py
+++ HomeWork#14/students.py
@@ -4,7 +4,7 @@ class Student:
     status = True
     pay = 1000
 
-    def __init__(self, first_name, last_name, age, grade):
+    def __init__(self,first_name,last_name,age,grade):
         
         self.first_name= first_name
         self.last_name= last_name
@@ -13,22 +13,22 @@ class Student:
         
 
     def get_full_name(self):
-        pass
+        
         return f"{self.first_name} {self.last_name}"  
 
     def get_discount(self):
-        pass
+        
         if self.age < 18:
-            Student.pay = 1000-(1000 * 0.2)
-        return Student.pay
+            self.pay = 1000*0.8
+        return self.pay
     
     def calculate_average(self):
-        pass
+        
         average =sum(self.grade)/len(self.grade)
         return average
     
     def get_status(self):
-        pass
+        
         if self.calculate_average()>90:
             return f"Excelent"
         elif self.calculate_average() > 70 and self.calculate_average() < 90:
@@ -45,8 +45,9 @@ class Student:
 student1=Student(first_name="Alex", last_name="Todua",age=34,grade=[80,75,96,85,95])
 student2=Student(first_name="John", last_name="Wick",age=21,grade=[71,76,85,90,81])
 student3=Student(first_name="Ana", last_name="De Armas",age=17,grade=[100,90,96,90,91])
-student4=Student(first_name="Joe", last_name="Doe",age=21,grade=[41,45,46,45,35])
+student4=Student(first_name="Joe", last_name="Doe",age=19,grade=[41,45,46,45,35])
 student5=Student(first_name="Dakota", last_name="Johnson",age=16,grade=[50,65,56,45,55])
+student6=Student(first_name="William", last_name="Baldwin",age=25,grade=[77,68,59,85,65])
 
 print(f"Student's full name: {student1.get_full_name()}")
 print(f"Cost of training: {student1.get_discount()}")
@@ -79,4 +80,10 @@ print()
 print(f"Student's full name: {student5.get_full_name()}")
 print(f"Cost of training: {student5.get_discount()}")
 print(f"Your grade point average is: {student5.calculate_average()} {student5.get_status()}")
+
+print()
+
+print(f"Student's full name: {student6.get_full_name()}")
+print(f"Cost of training: {student6.get_discount()}")
+print(f"Your grade point average is: {student6.calculate_average()} {student6.get_status()}")
 
