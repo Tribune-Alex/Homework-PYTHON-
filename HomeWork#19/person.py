825ce9e1e40d1254ecb5a6a8c0f4c98ae9520d54
Alex Todua <alx.todua777@gmail.com>
2025-11-25 10:51:46 +0400

    Adding homework files

---
+++ HomeWork#19/person.py
@@ -0,0 +1,32 @@
+import json
+
+def new_customer(n):
+    
+    with open('persons.json', "r") as file:
+        persons = json.load(file)
+
+   
+    if persons:
+        last_id = persons[-1]["id"]
+    else:
+        last_id = 0
+
+    for i in range(n):
+        name = input("Input your name: ").lower()
+        age = int(input("Inout your age: "))
+
+        new_person = {
+            "id": last_id + 1,
+            "name": name,
+            "age": age
+        }
+
+        persons.append(new_person)
+        last_id += 1
+
+    with open("persons.json", "w") as file:
+        json.dump(persons, file, indent=4)
+
+    print("You've just been added new customer in list")
+ 
+new_customer(2)
